2ae341366359136029d9fb56497c5ca6b5a8761e
jaykuhb <jakub.strelsky@gmail.com>
2017-01-01 18:45:29 +0100

    adding fourierNet with convolutional input and fully connected input

--- models.py
+++ models.py
@@ -2,6 +2,77 @@ import numpy as np
 import tensorflow as tf
 
 
+class FourierNet(object):
+
+    def __init__(self, filters = 256, n_hidden1=400, stride=1, n_input1=64,n_channels=1,n_input2=128, n_classes = 99):
+        super(FourierNet, self).__init__()
+        self.n_hidden1 = n_hidden1
+        self.n_classes = n_classes
+        self.n_input1 = n_input1
+        self.filters = filters
+        self.stride = stride
+        self.n_input2 = n_input2
+        self.n_channels = n_channels
+        
+        # tf Graph input/output
+        self.x1 = tf.placeholder("float", [None, n_input1*n_channels])
+        self.x2 = tf.placeholder("float", [None, n_input2])
+        self.y = tf.placeholder("float", [None, n_classes])
+        # Store layers weight & bias
+        # init ~ 1/sqrt(n) * random sample from normal distribution
+        # zero biases
+        self.weights = {
+            'c1': tf.Variable(tf.scalar_mul(tf.sqrt(1./float(4*n_channels)),
+                                            tf.random_normal([4,n_channels, self.filters]))),
+            'h1': tf.Variable(tf.scalar_mul(tf.sqrt(1./float((n_input1//stride)*self.filters+n_input2)),
+                                            tf.random_normal([(n_input1//stride)*self.filters+n_input2, self.n_hidden1]))),
+
+            'out': tf.Variable(tf.scalar_mul(tf.sqrt(1./float(self.n_hidden1)),
+                                            tf.random_normal([self.n_hidden1, self.n_classes])))
+        }
+        self.biases = {
+            'bc1': tf.Variable(tf.zeros([self.filters])),
+            'b1': tf.Variable(tf.zeros([self.n_hidden1])),
+            'out': tf.Variable(tf.zeros([self.n_classes]))
+        }
+
+        # function that computes output of neural net
+        self.predict = self.compute(self.x1, self.x2, self.weights, self.biases)
+
+        # loss function
+        # with L2-regularization term
+        self.cost = (tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(self.predict, self.y))
+                + 0.0001*tf.nn.l2_loss(self.weights['h1'])
+                + 0.0001*tf.nn.l2_loss(self.weights['c1'])
+                + 0.0001*tf.nn.l2_loss(self.weights['out'])
+                + 0.0001*tf.nn.l2_loss(self.biases['b1'])
+                + 0.0001*tf.nn.l2_loss(self.biases['bc1'])
+                + 0.0001*tf.nn.l2_loss(self.biases['out']))
+
+
+    def conv1D(x, W, b, stride=1):
+        # Conv1D wrapper, with bias and relu activation
+        x = tf.nn.conv1d(x, W, stride, padding="VALID")
+        x = tf.nn.bias_add(x, b)
+        return tf.nn.relu(x)
+
+    # Create model computation function
+    def compute(self,x1,x2, weights, biases):
+
+        x1 = tf.reshape(x1, shape=[-1, self.n_input1, self.n_channels])
+        act1 = tf.nn.conv1d(x1,weights['c1'],self.stride,padding="SAME")
+        act1 = tf.nn.bias_add(act1, biases['bc1'])
+        act1 = tf.nn.relu(act1)
+        #flatten
+        act1= tf.reshape(act1, shape = [-1,(self.n_input1//self.stride)*self.filters])
+        act1= tf.concat(1,[act1,x2])
+
+        act2= tf.add(tf.matmul(act1, weights['h1']), biases['b1'])
+        act2 =tf.nn.relu(act2)
+        # Output layer with linear activation
+        out = tf.add(tf.matmul(act2, weights['out']),biases['out'])
+        return out  
+
 
 class ConvNet1D2(object):
 
